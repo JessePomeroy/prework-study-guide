7947c12c1b6daf7e9e75f1e1ee2e37c4dddb987b
JessePomeroy <thinkingofview@gmail.com>
2023-03-26 18:48:41 -0500

    debugged the js so that our functions are called and prints in the console.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,45 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML.");
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS.");
+function listTopics() {
+    
+    for(var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 
-} else if (topic === 'Git') {
-    console.log("Let's study Git.");
-
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript.");
-
-} else {
-    console.log('Please try again.');
 }
 
-*/
+function selectTopics() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML.");
 
-for(var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS.");
+
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git.");
+
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript.");
+
+    } else {
+        console.log('Please try again.');
+    }
 }
+
+function listTopics() {
+    
+    for(var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+}
+
+
+console.log('Here are the topics we learned through Prework:');
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopics();
